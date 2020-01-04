80d972039907e0eaf8487ea76fc9d9d46b4b2c90
root <root@ip-172-31-8-57.eu-west-1.compute.internal>
2020-01-04 12:30:22 +0000

    2 test

---
+++ scripts/cake.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SCRIPT='../build.cake'
+CAKE_VERSION='0.35.0'
+
+
+# Install  cake.tool
+dotnet tool install --global cake.tool --version $CAKE_VERSION
+export PATH="$PATH:$HOME/.dotnet/tools"
+
+# Start Cake
+CAKE_ARGS="$SCRIPT -verbosity=verbose"
+
+echo "\035[32mdotnet cake $CAKE_ARGS $@"
+
+dotnet cake $CAKE_ARGS "$@"
