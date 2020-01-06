7d021b62ee6cefcdf8ea5a1f35d6cba1261a0f70
root <root@ip-172-31-8-57.eu-west-1.compute.internal>
2020-01-06 03:26:38 +0000

    6

--- cake.ps1
+++
@@ -1,9 +0,0 @@
-[string]$CAKE_SCRIPT_FILE = '../build.cake'
-[string]$CAKE_ARGS="$CAKE_SCRIPT_FILE -verbosity=verbose"
-dotnet --list-sdks
-dotnet tool install --global cake.tool --version $CAKE_VERSION
-export PATH="$PATH:$HOME/.dotnet/tools"
-
-dotnet tool restore
-Write-Host "dotnet cake  $CAKE_ARGS $ARGS" -ForegroundColor GREEN
-dotnet cake  $CAKE_ARGS $ARGS
